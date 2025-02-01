d81ad18f3d5315f708a774151959085590cfb9f0
Bhaskara Cheruku <bhaskara.cheruku@cognine.com>
2025-02-01 16:19:53 +0530

    Add project files.

---
+++ githubactions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace githubactions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
